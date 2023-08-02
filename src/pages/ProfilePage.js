94a91c9c784e6c3468208d2de3f28d910ac3df79
zZHugoZz <estruchhugo@gmail.com>
2023-08-02 18:09:57 +0200

    created the EditableProfileData component

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import useInterceptor from "../utils/useInterceptor";
+import EditableProfileData from "../components/EditableProfileData";
 
 const ProfilePage = () => {
+  const [profile, setProfile] = useState({});
+
+  const axiosInterceptor = useInterceptor();
+
+  useEffect(() => {
+    axiosInterceptor.get("/profiles").then((response) => {
+      setProfile(response.data);
+    });
+  }, []);
+
   return (
     <>
       <h2>Profile</h2>
+      <h3>profile picture: {profile.profile_picture}</h3>
+      <h3>Hello {profile.username}</h3>
+      <EditableProfileData profile={profile} />
     </>
   );
 };
