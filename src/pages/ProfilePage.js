28d291b1d8f957aea5d8881ceff72b63506b5428
zZHugoZz <estruchhugo@gmail.com>
2023-08-04 09:09:10 +0200

    fixed the profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -6,12 +6,16 @@ import EditableGender from "../components/EditableGender";
 
 const ProfilePage = () => {
   const [profile, setProfile] = useState({});
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
   const axiosInterceptor = useInterceptor();
 
   useEffect(() => {
     axiosInterceptor.get("/profiles").then((response) => {
       setProfile(response.data);
+      setUsername(response.data.user.username);
+      setEmail(response.data.user.email);
     });
   }, []);
 
@@ -19,8 +23,8 @@ const ProfilePage = () => {
     <>
       <h2>Profile</h2>
       <h3>profile picture: {profile.profile_picture}</h3>
-      <h3>Hello {profile.user.username}</h3>
-      <h3>email: {profile.user.email}</h3>
+      <h3>Hello {username}</h3>
+      <h3>email: {email}</h3>
       <EditableAge />
       <EditableGender />
     </>
