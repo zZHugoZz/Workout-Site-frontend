6374c39ba9e08613f9be0d83124b4ee5ba926461
zZHugoZz <estruchhugo@gmail.com>
2023-08-04 19:12:39 +0200

    added the useffects to fetch the bodyweight data and set the datasets and labels

--- src/components/weightChange.js
+++ src/components/weightChange.js
@@ -1,38 +1,45 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import WeightChart from "./WeightChart";
+import useInterceptor from "../utils/useInterceptor";
 
 const WeightChange = () => {
+  const [labels, setLabels] = useState([]);
   const [datasets, setDatasets] = useState([]);
+  const [unit, setUnit] = useState("");
   const [chartData, setChartData] = useState({
+    labels: labels,
     datasets: datasets,
   });
 
+  const axiosInterceptor = useInterceptor();
+
   useEffect(() => {
-    setDatasets([
-      {
-        data: [
-          {
-            x: "2021-11-06 23:39:30",
-            y: 50,
-          },
-          {
-            x: "2021-11-07 01:00:28",
-            y: 60,
-          },
-          {
-            x: "2021-11-07 09:00:28",
-            y: 20,
-          },
-        ],
-      },
-    ]);
-  });
+    axiosInterceptor.get("/bodyweights").then((response) => {
+      setLabels(response.data.map((bodyweight) => bodyweight.date));
+      setDatasets({
+        label: "Weight",
+        data: response.data.map((bodyweight) => bodyweight.weight),
+        boderColor: "green",
+        backgroundColor: "green",
+        color: "#AFC0CF",
+        tension: 0.1,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    setChartData({
+      labels: labels,
+      datasets: datasets,
+    });
+  }, [labels, datasets]);
 
   return (
     <>
       <h2>Weight change</h2>
-      <WeightChart data={chartData} />
+      <WeightChart />
+      <h2></h2>
     </>
   );
 };
