d7eb268a833d03b0d0006a31d22fa14cda7f446e
zZHugoZz <estruchhugo@gmail.com>
2023-08-04 14:03:45 +0200

    configured the WeightChart

--- src/components/WeightChart.js
+++ src/components/WeightChart.js
@@ -3,54 +3,59 @@ import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   LineElement,
-  CategoryScale,
   LinearScale,
   PointElement,
   Tooltip,
   Legend,
+  TimeScale,
 } from "chart.js";
+import "chartjs-adapter-date-fns";
+import format from "date-fns/format";
 
 ChartJS.register(
   LineElement,
-  CategoryScale,
+  TimeScale,
   LinearScale,
   PointElement,
   Tooltip,
   Legend
 );
 
-const WeightChart = ({ data }) => {
-  const options = {
-    responsive: true,
-    animation: false,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        labels: {
-          font: {
-            family: "Regular",
-          },
-        },
+const currentDate = new Date();
+
+const WeightChart = () => {
+  const data = {
+    labels: [
+      format(currentDate, "yyyy-MM-dd"),
+      format(new Date().setDate(currentDate.getDate() + 1), "yyyy-MM-dd"),
+      format(new Date().setDate(currentDate.getDate() + 2), "yyyy-MM-dd"),
+      format(new Date().setDate(currentDate.getDate() + 3), "yyyy-MM-dd"),
+    ],
+    datasets: [
+      {
+        label: "some label",
+        data: [1, 2, 1.5, 3],
+        backgroundColor: "aqua",
+        borderColor: "white",
+        tension: 0.4,
       },
-    },
+    ],
+  };
+  const options = {
     scales: {
       x: {
-        title: {
-          display: true,
-          text: "Sessions",
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: "weight",
+        type: "time",
+        time: {
+          unit: "day",
         },
       },
     },
   };
   return (
     <>
-      <Line data={data} options={options} />
+      <div>
+        <Line data={data} options={options} />
+      </div>
     </>
   );
 };
