327cdcc14fd076212d61c17dfca6b479c5adeffd
zZHugoZz <estruchhugo@gmail.com>
2023-08-06 18:49:15 +0200

    converted the date from the date calendar to yyyy-mm--dd format

--- src/components/WorkoutsCalendar.js
+++ src/components/WorkoutsCalendar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import dayjs from "dayjs";
 
@@ -10,13 +10,18 @@ import WorkoutDay from "./WorkoutDay";
 
 const WorkoutsCalendar = () => {
   const [highlightedDays, setHighlightedDays] = useState([4, 5, 14]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    console.log("value: ", dayjs(value.$d).format("YYYY-MM-DD"));
+  }, [value]);
 
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateCalendar
           sx={{ margin: "0" }}
-          onChange={(value) => console.log("value: ", value.$y)}
+          onChange={(value) => setValue(value)}
           defaultValue={dayjs()}
           slots={{
             day: WorkoutDay,
--- src/context/WorkoutsContext.js
+++ src/context/WorkoutsContext.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { createContext, useState } from "react";
 
+import useInterceptor from "../utils/useInterceptor";
+
 export const WorkoutsContext = createContext();
 
 export default WorkoutsProvider = ({ children }) => {
-  const contextData = {};
+  const [currentWorkout, setCurrentWorkout] = useState({});
+
+  const axiosInterceptor = useInterceptor();
+
+  const contextData = {
+    currentWorkout: currentWorkout,
+    setCurrentWorkout: setCurrentWorkout,
+  };
 
   return (
     <WorkoutsContext.Provider value={contextData}>
