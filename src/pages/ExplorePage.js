cbe4f52c2cb1d81b0bafce56e6aa82e015307acb
zZHugoZz <estruchhugo@gmail.com>
2023-08-06 10:05:29 +0200

    created the basic structure of the explore page

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -5,13 +5,34 @@ import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
+import {
+  backContent,
+  pecsContent,
+  shouldersContent,
+  armsContent,
+  absContent,
+  legsContent,
+} from "../components/ExplorePageContent";
+
 const ExplorePage = () => {
   return (
     <>
-      <Container maxWidth="sm">
-        <Stack direction="row" justifyContent="center">
+      <Container maxWidth="md" sx={{ marginTop: "50px" }}>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          sx={{ marginBottom: "50px" }}
+        >
           <Typography variant="h4">Overview</Typography>
           <ExploreSearchBar />
+        </Stack>
+        <Stack spacing={5}>
+          {backContent}
+          {pecsContent}
+          {shouldersContent}
+          {armsContent}
+          {absContent}
+          {legsContent}
         </Stack>
       </Container>
     </>
