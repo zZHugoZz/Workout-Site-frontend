6c2d4e0e85a50be1dd27f4e0d99aec665826777d
zZHugoZz <estruchhugo@gmail.com>
2023-08-10 18:50:33 +0200

    put the LightTooltip in its own file

--- src/components/manage/workouts/WorkoutExerciseTooltip.js
+++ src/components/manage/workouts/WorkoutExerciseTooltip.js
@@ -1,33 +1,18 @@
 import React, { useState, useContext } from "react";
-import axios from "axios";
 
 import LoadingButton from "@mui/lab/LoadingButton";
 import IconButton from "@mui/material/IconButton";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import Stack from "@mui/material/Stack";
 import EditIcon from "@mui/icons-material/Edit";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 
-import { styled } from "@mui/material/styles";
-
 import DeleteWorkoutExerciseButton from "./DeleteWorkoutExerciseButton";
+import { LightTooltip } from "../../../styles/LightTooltip";
 
 const WorkoutExerciseTooltip = ({ exerciseId }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-
-  const LightTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: "rgba(0, 0, 0, 0.87)",
-      boxShadow: theme.shadows[1],
-      fontSize: 11,
-    },
-  }));
 
   return (
     <>
