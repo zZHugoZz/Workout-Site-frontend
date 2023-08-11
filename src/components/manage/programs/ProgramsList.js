a269fcdbcbf918e52a22e11748c4110970fcb75e
zZHugoZz <estruchhugo@gmail.com>
2023-08-11 18:46:43 +0200

    turned the ProgramsPagination to the ProgramsList

--- src/components/manage/programs/ProgramsList.js
+++ src/components/manage/programs/ProgramsList.js
@@ -1,11 +1,55 @@
 import React from "react";
+import { useState, useEffect, useContext } from "react";
 
-import ProgramsPagination from "./ProgramsPagination";
+import Grid from "@mui/material/Unstable_Grid2";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+
+import Pagination from "@mui/material/Pagination";
+import usePagination from "../../../utils/pagination";
+import Program from "./Program";
+import { ProgramsContext } from "../../../context/ProgramsContext";
 
 const ProgramsList = () => {
+  const { programs } = useContext(ProgramsContext);
+
+  const [count, setCount] = useState(10);
+  const [page, setPage] = useState(1);
+  const perPage = 4;
+
+  const displayedData = usePagination(programs, perPage);
+
+  const handleChange = (e, page) => {
+    setPage(page);
+    displayedData.jump(page);
+  };
+
+  useEffect(() => {
+    setCount(Math.ceil(programs.length / perPage));
+  }, [programs]);
+
   return (
     <>
-      <ProgramsPagination />
+      <Grid
+        container
+        spacing={2}
+        sx={{ marginLeft: "10px", marginRight: "10px", height: "200px" }}
+      >
+        {displayedData.currentData().map((program) => (
+          <Program key={program.id} program={program} />
+        ))}
+      </Grid>
+      <Divider sx={{ paddingTop: "20px" }} />
+      <Container>
+        <Pagination
+          count={count}
+          page={page}
+          variant="outlined"
+          shape="rounded"
+          onChange={handleChange}
+          sx={{ paddingTop: "20px" }}
+        />
+      </Container>
     </>
   );
 };
