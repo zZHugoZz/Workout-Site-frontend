b6f58f333d61ebf0cdb1deeee746c534da52b171
zZHugoZz <estruchhugo@gmail.com>
2023-08-13 10:21:30 +0200

    integrated the custom dialog to the Settings component

---
+++ src/components/headers/SelectUnit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectUnit = () => {
+  return <div>SelectUnit</div>;
+};
+
+export default SelectUnit;
--- src/components/headers/Settings.js
+++ src/components/headers/Settings.js
@@ -2,13 +2,8 @@ import React, { useState } from "react";
 
 import IconButton from "@mui/material/IconButton";
 import SettingsIcon from "@mui/icons-material/Settings";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+
+import CustomDialog from "../../utils/CustomDialog";
 
 const Settings = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,11 +13,11 @@ const Settings = () => {
       <IconButton color="inherit" onClick={() => setIsOpen(true)}>
         <SettingsIcon />
       </IconButton>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <DialogTitle>
-          <Stack></Stack>
-        </DialogTitle>
-      </Dialog>
+      <CustomDialog
+        title="Settings"
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      ></CustomDialog>
     </>
   );
 };
